814cd586d4f9695c716adc373d72726ff25d1c9c
AkinoKawaii <canarianara@gmail.com>
2017-09-09 11:57:33 +0700

    Update onn.py

--- onn.py
+++ onn.py
@@ -10,7 +10,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence( game=discord.Game( name="Onika Studio", type = 1))
+    await client.change_presence( game=discord.Game( name="Onika Studio", type = 0))
     
 ###############SAY###############
 @client.event
