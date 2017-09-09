866f557ac525a5876e13ea85e47b332671ebf604
AkinoKawaii <canarianara@gmail.com>
2017-09-09 12:14:30 +0700

    Update onn.py

--- onn.py
+++ onn.py
@@ -12,100 +12,6 @@ async def on_ready():
     print('------')
     await client.change_presence( game=discord.Game( name="Onika Studio", type = 1))
     
-###############SAY###############
-@client.event
-async def on_message(message):
-    random_color = colors()
- #   if message.content.startswith('=ok'):
-     #   await client.send_message(message.channel,'ok')
-    if message.content.startswith('orders'):
-        await client.send_message(message.channel,"""```         
-Hi Im manage this server
-```""")
-      
-    elif message.content.startswith('.o'):
-        try:
-            if message.channel.id !='274387797140570112':
-                coloursman = int(random_color['color'])
-                if message.content == 'h!user':
-                    discord.Member = None
-                    author = message.author
-                    if not discord.Member :
-                        discord.Member = message.author
-                    roles = [x.name for x in discord.Member.roles if x.name != "@everyone"]
-                    if not roles: roles = ["None"]
-                    data = "\n"
-                    data += "Name: {}\n".format(escape_mass_mentions(str(discord.Member)))
-                    data += "Nickname: {0}\n".format(discord.Member.display_name)
-                    data += "ID: {}\n".format(discord.Member.id)
-                    passed = (message.timestamp - discord.Member.created_at).days
-                    data += "Created: {} days ago.\n".format(passed)
-                    passed = (message.timestamp - discord.Member.joined_at).days
-                    data += "Joined: {} days ago\n".format(passed)
-                    data += "Server: {}\n".format(discord.Member.server)
-                    data += "Status: {}\n".format(discord.Member.status)
-                    data += "Playing: {}\n".format(discord.Member.game)
-                    data += "Bot Account: {}\n".format(discord.Member.bot)
-                    data += "Roles: {}\t".format(", ".join(roles))
-                    data += "**({0})**\n".format(len(roles))
-                    data += "Top Role: {}\n\n".format(discord.Member.top_role)
-                
-                    user_em = discord.Embed(
-                    description=data)
-                    user_em.set_author(
-                    name="Your Status :")
-                    user_em.set_image(url = discord.Member.avatar_url)
-                    user_em.color=coloursman
-                
-                    await client.send_message(message.channel,embed = user_em)          
-           
-           
-           
-                else:
-                    check_tagger = re.findall('.o\s(.*)',message.content)
-        
-                    hilol_ = ' '.join(check_tagger)
-                    no1is = re.sub(r'[^\w]', ' ', hilol_)
-                    thisiscool = no1is.replace(' ','')
-                
-                    member_id = message.author.server.get_member(user_id = thisiscool)
-                    
-                    discord.Member = member_id 
-                    roles = [x.name for x in discord.Member.roles if x.name != "@everyone"]
-                    if not roles: roles = ["None"]
-                    data = "\n"
-                    data += "Name: {}\n".format(escape_mass_mentions(str(discord.Member)))
-                    data += "ID: {}\n".format(discord.Member.id)
-                    passed = (message.timestamp - discord.Member.created_at).days
-                    data += "Created: {} days ago\n".format(passed)
-                    passed = (message.timestamp - discord.Member.joined_at).days
-                    data += "Joined: {} days ago\n".format(passed)
-                    data += "Server: {}\n".format(discord.Member.server)
-                    data += "Status: {}\n".format(discord.Member.status)
-                    data += "Playing: {}\n".format(discord.Member.game)
-                    data += "Bot Account: {}\n".format(discord.Member.bot)
-                    data += "Roles: {}\t".format(", ".join(roles))
-                    data += "**({0})**\n".format(len(roles))
-                    data += "Top Role: {}\n\n".format(discord.Member.top_role)
-                    user_em = discord.Embed(
-                    description=data)
-                    user_em.set_author(
-                    name="User Status :")
-                    user_em.set_image(url = discord.Member.avatar_url)
-                    user_em.color=coloursman
-                
-                    await client.send_message(message.channel,embed = user_em)
-  
-        except Exception as e: print (str(e))
-                  
-    elif message.content.startswith('.osay'):
-        if message.content == '.osay':return
-        else:
-            text = re.findall('.osay\s(.*)',message.content)
-            refined = ' '.join(text)
-            await client.send_message(message.channel, '{0}'.format(refined))
-
-##########streamer##########
 @client.event
 async def on_member_update(before, after):
         server = after.server
@@ -131,24 +37,16 @@ async def on_member_update(before, after):
                         embed = discord.Embed(description='{0.display_name} ({0.id}) got the {1.name} role'.format(before, role))
                         embed.set_author(name='Role applied', icon_url=member.avatar_url)
                         await client.send_message(after.server, embed=embed)
-
-##########join and leave##########
+                        
+@client.event
+async def on_member_remove(member):
+    server = member.server
+    fmt = '{0.mention} leave the {1.name} server'
+    await client.send_message(server, fmt.format(member, server))
 @client.event
 async def on_member_join(member):
     server = member.server
     fmt = 'Welcome {0.mention} to {1.name}!'
-    await client.send_message(server, fmt.format(member, server))
- 
-@client.event
-async def on_member_remove(member):
-    server = member.server
-    fmt = '{0.mention} has left {1.name}!'
-    await client.send_message(server, fmt.format(member, server))
- 
-@client.event
-async def on_member_ban(member):
-    server = member.server
-    fmt = '{0.member} has been banned'
     await client.send_message(server, fmt.format(member, server))
     
     print('Starting....')
