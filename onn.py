5a3f5b00ba2b751b8bc50a0f266f87a7aaacea2a
AkinoKawaii <canarianara@gmail.com>
2017-09-10 08:00:42 +0700

    Update onn.py

--- onn.py
+++ onn.py
@@ -123,13 +123,11 @@ async def on_member_update(before, after):
         server = after.server
         member = after
         if before.name != after.name:
-            fmt = ':warning: From {0.name} to :white_check_mark: {1.name}'
+            fmt = ':warning: `{0.name}` *changed their name to* `{1.name}`'
             await client.send_message(after.server, fmt.format(before, after))
         if before.nick != after.nick:
-            embed = discord.Embed(description='From {0.nick} ({0.id}) to {1.nick}'.format(before, after))
-            embed.set_author( name='Nick Changed', icon_url=member.avatar_url)
-            await client.send_message(after.server, embed=embed)
-        if before.roles != after.roles:
+            fmt = icon_url=member.avatar_url':warning: `{0.nick}` *changed their nick to* `{1.nick}`'
+            await client.send_message(after.server, fmt.format(before, after))
             if len(before.roles) > len(after.roles):
                 for role in before.roles:
                     if role not in after.roles:
