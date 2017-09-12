1caedb19edc14728d8341da8f422d0767d75d341
AkinoKawaii <canarianara@gmail.com>
2017-09-12 18:31:03 +0700

    Update onn.py

--- onn.py
+++ onn.py
@@ -2,6 +2,7 @@ import discord
 import asyncio
 import re
 import random
+import talking
 client = discord.Client()
 def colors():
     list_colors = [{'color':"16777215"},{'color':"16711680"},{'color':"0"},{'color':"255"},{'color':"8388736"},{'color':"65535"},{'color':"8421504"},{'color':"8421504"},{'color':"10040319"}]
